dc3ebf7d4f5062d5f19e22a1b46c70be4807bd93
Krolebord <krolebord@gmail.com>
2021-07-27 23:59:55 +0300

    Add Playlist entity

---
+++ backend/Perflow.Studio/Business/Playlists/Mapping/PlaylistProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Perflow.Studio.Business.Playlists.DTOs;
+using Perflow.Studio.Domain.Entities;
+
+namespace Perflow.Studio.Business.Playlists.Mapping
+{
+    public class PlaylistProfile : Profile
+    {
+        public PlaylistProfile()
+        {
+            CreateMap<Playlist, PlaylistReadDTO>();
+            CreateMap<PlaylistWriteDTO, Playlist>();
+        }
+    }
+}
