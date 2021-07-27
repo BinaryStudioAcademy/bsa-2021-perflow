768f1f9ca6af7956ce779424b5f57376329e5fb8
Krolebord <krolebord@gmail.com>
2021-07-28 00:00:05 +0300

    Add User entity

---
+++ backend/Perflow.Studio/Business/Users/DTOs/UserWriteDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Perflow.Studio.Business.Users.DTOs
+{
+    public record UserWriteDTO(
+        DateTimeOffset? Birthday,
+        string? Description,
+        string? IconURL
+    );
+}
---
+++ backend/Perflow.Studio/Business/Users/Mapping/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Perflow.Studio.Business.Users.DTOs;
+using Perflow.Studio.Domain.Entities;
+
+namespace Perflow.Studio.Business.Users.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserReadDTO>();
+            CreateMap<UserWriteDTO, User>();
+        }
+    }
+}
