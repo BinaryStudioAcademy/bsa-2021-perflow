13b9d2dcd5e8dd5eeade05325988263ba8b4ca45
Krolebord <krolebord@gmail.com>
2021-07-27 23:59:26 +0300

    Add Album entity

---
+++ backend/Perflow.Studio/Business/Albums/Handlers/UpdateAlbumHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+using Perflow.Studio.Business.Albums.Commands;
+
+namespace Perflow.Studio.Business.Albums.Handlers
+{
+    public class UpdateAlbumHandler : IRequestHandler<UpdateAlbumCommand, OneOf<Success, NotFound>>
+    {
+        public Task<OneOf<Success, NotFound>> Handle(UpdateAlbumCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
