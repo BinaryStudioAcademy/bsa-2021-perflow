f28b0abe016a91d2a40bcfb9aacf35cecbabe6c4
Khrapal Maksym <khrapal_m_g@outlook.com>
2021-07-29 01:04:06 +0300

    Deleted commit in code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{ path: '', loadChildren: () => import('./moduls/auth/auth.module').then(m => m.AuthModule )},
-{ path: 'main', loadChildren: () => import('./moduls/main/main.module').then(m => m.MainModule )},
-{ path: '**', redirectTo: '' },
-// { path: 'search', loadChildren: () => import('./moduls/search/search.module').then(m => m.SearchModule) },
-// { path: 'playlists', loadChildren: () => import('./moduls/playlists/playlists.module').then(m => m.PlaylistsModule) },
-// { path: 'songs', loadChildren: () => import('./moduls/songs/songs.module').then(m => m.SongsModule) }
+  {
+    path: '',
+    loadChildren: () =>
+      import('./moduls/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./moduls/main/main.module').then((m) => m.MainModule),
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
