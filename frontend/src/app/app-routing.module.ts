a618f8821fa202e6ee2c03d49387897b189eae6d
Slawisch <dmitry.js01@gmail.com>
2021-07-28 19:07:48 +0300

    Add ESLint

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const ROUTES: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -6,30 +6,31 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const FIXTURE = TestBed.createComponent(AppComponent);
+    const APP = FIXTURE.componentInstance;
+    expect(APP).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+  it('should have as title \'frontend\'', () => {
+    const FIXTURE = TestBed.createComponent(AppComponent);
+    const APP = FIXTURE.componentInstance;
+    expect(APP.title).toEqual('frontend');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    const FIXTURE = TestBed.createComponent(AppComponent);
+    FIXTURE.detectChanges();
+    const COMPILED = FIXTURE.nativeElement as HTMLElement;
+    expect(COMPILED.querySelector('.content span')?.textContent)
+      .toContain('frontend app is running!');
   });
 });
