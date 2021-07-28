71370d61ab6a82f18e32371d817044e0dcc3efcc
Khrapal Maksym <khrapal_m_g@outlook.com>
2021-07-28 14:48:44 +0300

    Create base left side menu bar
    Added base settings in sass.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialComponentsModule } from './common/material-component';
+import { MaterialComponentsModule } from './common/material-components.module';
 
 @NgModule({
   declarations: [
