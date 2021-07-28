4e280d33adffc1330822593dcbbcd4174d0a2456
alls77 <alina_selina@bk.ru>
2021-07-28 15:00:51 +0300

    add Azure Blob Storage

--- backend/Perflow/Startup.cs
+++ backend/Perflow/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Perflow.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +33,8 @@ namespace Perflow
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Perflow", Version = "v1" });
             });
+
+            services.AddBlobStorage(Configuration.GetConnectionString("BlobStorage"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
