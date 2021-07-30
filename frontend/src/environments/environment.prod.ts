871b5b9120d08a92d08fe62cdfa095a3494155b3
unknown <vik.vlasov.dev@gmail.com>
2021-07-30 21:08:52 +0300

    Add to environment.prod firebase creadential

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2BkmAvCNrKPrC97fWLCsWaKpNUODaZNw",
+    authDomain: "perflow-5d819.firebaseapp.com",
+    databaseURL: "https://perflow-5d819.firebaseio",
+    projectId: "perflow-5d819",
+    storageBucket: "perflow-5d819.appspot.com",
+    messagingSenderId: "194291102756",
+    appId: "xxxxx",
+    measurementId: "xxxxxxxxxxxxxxxx"
+  }
 };
