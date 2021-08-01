80e3fb5e9acd1a62497f7eeb65901363548e77f4
sleepergoose <sleepergoose@gmail.com>
2021-08-01 22:48:32 +0300

    Finished main page

--- frontend/src/app/moduls/main/main-home/main-home.component.ts
+++ frontend/src/app/moduls/main/main-home/main-home.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -7,58 +8,92 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./main-home.component.sass']
 })
 export class MainHomeComponent implements OnInit {
-  bar = 1;
-  newestCounter: number = 1;
-  albums = [{ pic: '../../../assets/album.jpg', name: '1', desc: '1' },
-    { pic: '../../../assets/album.jpg', name: '2', desc: '2' },
-    { pic: '../../../assets/album.jpg', name: '3', desc: '3' },
-    { pic: '../../../assets/album.jpg', name: '4', desc: '4' },
-    { pic: '../../../assets/album.jpg', name: '5', desc: '5' }];
-  currentAlbum: IAlbum = this.albums[0];
+  private _newestCounter: number = 1;
+  private _newestFiveAlbums = new Array<object>(); // Top 5 the newest albums. it's necessary to add  {{name}} to .html
+  public currentNewestAlbum = this._newestFiveAlbums[0];
+  public recentlyPlayed = new Array<object>(); // Only 8 items
+  public newReleases = new Array<object>();
+  public calmRhythms = new Array<object>();
+  public yourMix = new Array<object>();
+  public top100Songs = new Array<object>();
   constructor(private _authService: AuthService) {
   }
   ngOnInit() {
     this.hideProfileMenu();
-    this.resizer();
+    this._newestFiveAlbums = this.getNewestFiveAlbums();
+    this.recentlyPlayed = this.getRecentlyPlayed();
+    this.newReleases = this.getNewReleases();
+    this.calmRhythms = this.getCalmRhythms();
+    this.yourMix = this.getYourMix();
+    this.top100Songs = this.getTop100Songs();
   }
   logout() {
     this._authService.logout();
   }
-  resizer() {
-    // RESIZER
-    this.bar = 1;
-  }
   hideProfileMenu() {
-    this.bar = 2;
     window.onclick = (event) => {
       if (!(event.target as HTMLElement).matches('.profile')
         && !(event.target as HTMLElement).matches('.avatar')
         && !(event.target as HTMLElement).matches('.icon')) {
-        const el = document.getElementById('profile-menu');
-        if (el?.classList.contains('show')) {
-          el?.classList.remove('show');
+        const element = document.getElementById('profile_menu');
+        if (element?.classList.contains('show')) {
+          element?.classList.remove('show');
         }
       }
     };
   }
   showProfileMenu() {
-    this.bar = 2;
-    document.getElementById('profile-menu')?.classList.toggle('show');
+    document.getElementById('profile_menu')?.classList.toggle('show');
+  }
+  isRightRole(): boolean {
+    // Determines if the menu item 'Perflow Studio' will be shown in Profile menu
+    // 'Perflow Studio' - available only for artists and moderators
+    return false;
+  }
+  playAlbum() {
+    // Ability to play album
+  }
+  saveAlbum() {
+    // Ability to save album - album is saved in the user playlist
+  }
+  getNewestFiveAlbums(): Array<object> {
+    // User should be able to reach Top 5 of the newest albums
+    return new Array<object>();
+  }
+  getRecentlyPlayed(): Array<object> {
+    // User should be able to play Recently played - 8 songs/albums/playlists which was played recently
+    return new Array<object>();
+  }
+  getNewReleases(): Array<object> {
+    // User should be able to play New Releases - songs/albums which was added to system during last month
+    return new Array<object>();
+  }
+  getCalmRhythms(): Array<object> {
+    // User should be able to play Calm rhythms - the newest playlists which moderator creates
+    return new Array<object>();
+  }
+  getYourMix(): Array<object> {
+    // User should be able to play Your mix - take songs from all playlists plus liked songs and show in random order
+    return new Array<object>();
+  }
+  getTop100Songs(): Array<object> {
+    // User should be able to play Top 100 songs - 100 song ordering by amount of likes by all users
+    return new Array<object>();
   }
   nextSlide() {
     MainHomeComponent.accordionAnimation();
-    this.currentAlbum = this.albums[this.newestCounter];
-    this.newestCounter += 1;
-    if (this.newestCounter > 4) {
-      this.newestCounter = 0;
+    this.currentNewestAlbum = this._newestFiveAlbums[this._newestCounter];
+    this._newestCounter += 1;
+    if (this._newestCounter > 4) {
+      this._newestCounter = 0;
     }
   }
   previousSlide() {
     MainHomeComponent.accordionAnimation();
-    this.currentAlbum = this.albums[this.newestCounter];
-    this.newestCounter -= 1;
-    if (this.newestCounter < 0) {
-      this.newestCounter = 4;
+    this.currentNewestAlbum = this._newestFiveAlbums[this._newestCounter];
+    this._newestCounter -= 1;
+    if (this._newestCounter < 0) {
+      this._newestCounter = 4;
     }
     MainHomeComponent.accordionAnimation();
   }
@@ -71,14 +106,7 @@ export class MainHomeComponent implements OnInit {
     });
   }
   scroll(id: string) {
-    this.bar = 2;
-    const el = document.getElementById(id);
-    el?.scrollBy({ left: 270, behavior: 'smooth' });
+    const element = document.getElementById(id);
+    element?.scrollBy({ left: 270, behavior: 'smooth' });
   }
-}
-
-interface IAlbum {
-  pic: string;
-  name: string;
-  desc: string
 }
