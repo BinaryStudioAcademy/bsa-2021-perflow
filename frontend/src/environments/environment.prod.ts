ae86ab8acc74c65e7a0a314234d5f18a1ecdd252
unknown <vik.vlasov.dev@gmail.com>
2021-08-02 14:58:16 +0300

    Remove creadentials from frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,14 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA2BkmAvCNrKPrC97fWLCsWaKpNUODaZNw",
-    authDomain: "perflow-5d819.firebaseapp.com",
-    databaseURL: "https://perflow-5d819.firebaseio",
-    projectId: "perflow-5d819",
-    storageBucket: "perflow-5d819.appspot.com",
-    messagingSenderId: "194291102756",
-    appId: "xxxxx",
-    measurementId: "xxxxxxxxxxxxxxxx"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
   },
   apiUrl: '',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: "AIzaSyA2BkmAvCNrKPrC97fWLCsWaKpNUODaZNw",
-    authDomain: "perflow-5d819.firebaseapp.com",
-    databaseURL: "https://perflow-5d819.firebaseio",
-    projectId: "perflow-5d819",
-    storageBucket: "perflow-5d819.appspot.com",
-    messagingSenderId: "194291102756",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
   },
   apiUrl: 'https://localhost:44344',
 };
