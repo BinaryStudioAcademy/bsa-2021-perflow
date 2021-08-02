e69db5a70015cc2b3d0dc4c7cbac5deaf59b3465
Jeki <ambrejine00@gmail.com>
2021-08-02 08:09:51 +0300

    Add input validation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  showPassword : boolean = false;
+  showPassword: boolean = false;
+  isSignUpClicked: boolean = false;
+  loginForm = new FormGroup({
+    login: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern("^[a-zA-Z0-9@._\-]*$")])
+  });
 
-  constructor() { }
+  constructor() { 
+    this.loginForm.valueChanges.subscribe( changes => {
+      this.isSignUpClicked = false;
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  signUpOnClick()
+  {
+    this.isSignUpClicked = true;
   }
 
   toggleShowPassword() : void{
