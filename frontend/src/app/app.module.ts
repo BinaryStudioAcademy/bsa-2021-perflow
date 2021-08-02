e96765aef6fc5a065cfc5f330b78ba17264eca01
alls77 <alina_selina@bk.ru>
2021-08-02 21:12:05 +0300

    delete import duplicates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SongImageComponent } from './components/shared/upload/song-image/song-image.component';
