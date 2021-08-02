82b2db031a06a2ac0f62f830d514828716821cdd
Khrapal Maksym <khrapal_m_g@outlook.com>
2021-08-02 10:26:33 +0300

    testing

--- frontend/src/app/moduls/main/playlists-all/all-playlists.component.ts
+++ frontend/src/app/moduls/main/playlists-all/all-playlists.component.ts
@@ -10,7 +10,7 @@ export class AllPlaylistsComponent {
 // TODO: Here insert array count songs
   likedSongs: number = 256;
 // TODO: tempolary albums
-  albums: Album[] = [
+  allTemplary: Album[] = [
     {
       id: 1,
       titleImage: './../../../../assets/tepolary-images/card-title-1.png',
@@ -45,7 +45,7 @@ export class AllPlaylistsComponent {
       ]
     },
     {
-      id: 3,
+      id: 4,
       titleImage: './../../../../assets/tepolary-images/card-title-4.png',
       nameAlbum: "Relax work",
       songs: [
@@ -54,6 +54,97 @@ export class AllPlaylistsComponent {
         "SigalaPink",
         "Oximer"
       ]
+    }, 
+    {
+      id: 5,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-1.png',
+      nameAlbum: "Fresh & Chill",
+      songs: [
+        "Ed Sheeran",
+        "Paloma Mami Maroon 5",
+        "SigalaPink",
+        "Oximer"
+      ]
+    },
+    {
+      id: 6,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-2.png',
+      nameAlbum: "Imagine Dragons",
+      songs: [
+        "Believer",
+        "Natural",
+        "Thunder",
+        "Bad Liar"
+      ]
+    },
+    {
+      id: 7,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-3.png',
+      nameAlbum: "Tropical chaos",
+      songs: [
+        "Ed Sheeran",
+        "Paloma Mami Maroon 5",
+        "SigalaPink",
+        "Oximer"
+      ]
+    },
+    {
+      id: 8,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-4.png',
+      nameAlbum: "Relax work",
+      songs: [
+        "Ed Sheeran",
+        "Paloma Mami Maroon 5",
+        "SigalaPink",
+        "Oximer"
+      ]
     },   
+    {
+      id: 9,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-1.png',
+      nameAlbum: "Fresh & Chill",
+      songs: [
+        "Ed Sheeran",
+        "Paloma Mami Maroon 5",
+        "SigalaPink",
+        "Oximer"
+      ]
+    },
+    {
+      id: 10,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-2.png',
+      nameAlbum: "Imagine Dragons",
+      songs: [
+        "Believer",
+        "Natural",
+        "Thunder",
+        "Bad Liar"
+      ]
+    },
+    {
+      id: 11,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-3.png',
+      nameAlbum: "Tropical chaos",
+      songs: [
+        "Ed Sheeran",
+        "Paloma Mami Maroon 5",
+        "SigalaPink",
+        "Oximer"
+      ]
+    },
+    {
+      id: 12,
+      titleImage: './../../../../assets/tepolary-images/my-playlists-1.png',
+      nameAlbum: "Relax work",
+      songs: [
+        "Ed Sheeran",
+        "Paloma Mami Maroon 5",
+        "SigalaPink",
+        "Oximer"
+      ]
+    },     
   ];
+
+  albums = this.allTemplary.slice(0, 4);
+  playlists = this.allTemplary.slice(4, 11);
 }
