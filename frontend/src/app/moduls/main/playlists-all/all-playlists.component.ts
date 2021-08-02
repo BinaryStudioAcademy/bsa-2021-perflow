d7b2794e379907690f81826e413482f7033d997e
Khrapal Maksym <khrapal_m_g@outlook.com>
2021-08-02 05:34:19 +0300

    testing

--- frontend/src/app/moduls/main/playlists-all/all-playlists.component.ts
+++ frontend/src/app/moduls/main/playlists-all/all-playlists.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AllPlaylistsComponent {
 // TODO: Here insert array count songs
   likedSongs: number = 256;
+// TODO: tempolary albums
+  images: string[] = [
+    './../../../../assets/tepolary-images/Card 1.png',
+    './../../../../assets/tepolary-images/Card 2.png',
+    './../../../../assets/tepolary-images/Card 3.png',
+    './../../../../assets/tepolary-images/Card 4.png'   
+  ];
 }
