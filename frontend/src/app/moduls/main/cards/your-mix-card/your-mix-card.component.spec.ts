251d30b7657d4776e02c7fefa2538565f1f5c486
Ivan Slobodchykov <sleepergoose@gmail.com>
2021-08-03 13:21:33 +0300

    Feature/create main component (#20)
    * Added main layout - hat, sections
    
    * Added accordion, dropdown menu
    
    * Remade main page layout
    
    * Made main layout and cards
    
    * Fix responsability
    
    * Combine main page and left side menu
    
    * Finished main page
    
    * Fixed some bugs
    
    * Fixed bag in sass main page
    
    * Changed main-menu.component.sass
    
    * Fixed review notes
    
    * Made the profile menu a separate component

---
+++ frontend/src/app/moduls/main/cards/your-mix-card/your-mix-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { YourMixCardComponent } from './your-mix-card.component';
+
+describe('YourMixCardComponent', () => {
+  let component: YourMixCardComponent;
+  let fixture: ComponentFixture<YourMixCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [YourMixCardComponent]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(YourMixCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
