4abc79fa023022c73dcc753e13e38a4a91eb17a4
Jeki <ambrejine00@gmail.com>
2021-08-04 17:37:19 +0300

    Move firebase module and shared directives to shared module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,36 +5,22 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { SongToolbarComponent } from './components/shared/song-toolbar/song-toolbar.component';
-import { SongImageComponent } from './components/shared/upload/song-image/song-image.component';
-import { ShowHideDirective } from './directives/shared/show/show-hide.directive';
-import { DragDropDirective } from './directives/shared/upload/song-image/drag-drop.directive';
 import { AuthModule } from './modules/auth/auth.module';
-import { SongRowComponent } from './components/shared/song-row/song-row.component';
-import { SemanticDropdownDirective } from './directives/shared/dropdown/semantic-dropdown.directive';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FireBaseModule } from './shared/firebase-auth/firebase-auth.module';
 
 import { JwtInterceptor } from './interceptors/jwt/jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    SongImageComponent,
-    DragDropDirective,
-    SongToolbarComponent,
-    ShowHideDirective,
-    SongRowComponent,
-    SemanticDropdownDirective
+    LandingPageComponent
   ],
   imports: [
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    FireBaseModule,
     BrowserAnimationsModule,
     AuthModule,
     HttpClientModule
--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -1,24 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SongRowComponent } from './song-row/song-row.component';
-import { SongToolbarComponent } from './song-toolbar/song-toolbar.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 import { SongImageComponent } from './upload/song-image/song-image.component';
-
-
+import { SongToolbarComponent } from './song-toolbar/song-toolbar.component';
+import { SongRowComponent } from './song-row/song-row.component';
+import { DragDropDirective } from './directives/upload/song-image/drag-drop.directive';
+import { ShowHideDirective } from './directives/show/show-hide.directive';
+import { SemanticDropdownDirective } from './directives/dropdown/semantic-dropdown.directive';
 
 @NgModule({
   declarations: [
     SongRowComponent,
     SongToolbarComponent,
-    SongImageComponent
+    SongImageComponent,
+    DragDropDirective,
+    ShowHideDirective,
+    SemanticDropdownDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
-  exports:[
+  exports: [
     SongRowComponent,
     SongToolbarComponent,
-    SongImageComponent
+    SongImageComponent,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ]
 })
 export class SharedModule { }
