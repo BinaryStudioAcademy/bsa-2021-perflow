58fc09d46bb71e148ee62940f91e513d758b6f26
Jeki <ambrejine00@gmail.com>
2021-08-04 19:18:15 +0300

    rename guard folder to guards

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
