d0133eed2ee3712e27bc792a42e768a46b2687a0
Nikita Krasnov <nikita.krasnov@binary-studio.com>
2021-08-06 21:06:25 +0300

    Disabled auth guard

--- frontend/src/app/modules/main/main-routing.module.ts
+++ frontend/src/app/modules/main/main-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from 'src/app/guards/auth.guard';
+// import { AuthGuard } from 'src/app/guards/auth.guard';
 import { MainHomeComponent } from './main-home/main-home.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { PlaylistComponent } from './playlist/playlist.component';
@@ -10,11 +10,11 @@ import { SongsComponent } from './songs/songs.component';
 const routes: Routes = [{
   path: '',
   component: MainMenuComponent,
-  canActivate: [AuthGuard],
+  // canActivate: [AuthGuard],
   children: [
     {
       path: '',
-      canActivateChild: [AuthGuard],
+      // canActivateChild: [AuthGuard],
       children: [
         { path: 'main', component: MainHomeComponent },
         { path: 'search', component: SearchComponent },
