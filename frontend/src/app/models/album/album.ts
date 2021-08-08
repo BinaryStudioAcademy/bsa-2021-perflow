92308cc432bf463e127cabe5fbca7b88e9c60eb3
alls77 <alina_selina@bk.ru>
2021-08-08 12:36:09 +0300

    update header

--- frontend/src/app/models/album/album.ts
+++ frontend/src/app/models/album/album.ts
@@ -1,5 +1,12 @@
+import { User } from "../user/user";
+
 export interface Album {
+  id: number;
   name: string;
+  iconURL: string;
+  description: string;
+  artist: User; 
+  releaseYear: number;
 
   // TODO: add fields
 }
