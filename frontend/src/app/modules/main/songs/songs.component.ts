56fb772ce4a500d574335c6ed52248c3ffafd0ce
Ivan Slobodchykov <sleepergoose@gmail.com>
2021-08-11 00:11:44 +0300

    Added ability to dislike liked songs (#54)

--- frontend/src/app/modules/main/songs/songs.component.ts
+++ frontend/src/app/modules/main/songs/songs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ReactionService } from 'src/app/services/reaction.service';
 import { Song } from '../../../models/song/song';
 import { SongsService } from '../../../services/songs/songs.service';
 
@@ -11,12 +14,18 @@ import { SongsService } from '../../../services/songs/songs.service';
 export class SongsComponent implements OnInit {
   songs: Song[] = [];
 
+  private _userId: number;
+
   constructor(
-    private _songService: SongsService
+    private _songService: SongsService,
+    private _authService: AuthService,
+    private _reactionService: ReactionService
   ) {}
 
   ngOnInit(): void {
     this.loadLikedSongs();
+
+    this.getUserId();
   }
 
   loadLikedSongs() {
@@ -25,5 +34,22 @@ export class SongsComponent implements OnInit {
         this.songs = songs;
       }
     );
+  }
+
+  getUserId() {
+    this._authService.getAuthStateObservable()
+      .pipe(filter((state) => state !== null))
+      .subscribe((authState) => {
+        this._userId = authState!.id;
+      });
+  }
+
+  dislikeSong(songId: number) {
+    this._reactionService.removeLike(songId, this._userId)
+      .subscribe({
+        next: () => {
+          this.songs = this.songs.filter((s) => s.id !== songId);
+        }
+      });
   }
 }
