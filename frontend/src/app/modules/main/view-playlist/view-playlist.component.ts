eea7194f50e6fb2230fb96fc68f2ddd58fa29bbb
Khrapal Maksym <72307872+Khrapal-Max@users.noreply.github.com>
2021-08-12 11:24:13 +0300

    pause

--- frontend/src/app/modules/main/view-playlist/view-playlist.component.ts
+++ frontend/src/app/modules/main/view-playlist/view-playlist.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Song } from 'src/app/models/song/song';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ReactionService } from 'src/app/services/reaction.service';
+import { SongsService } from 'src/app/services/songs/songs.service';
 
 @Component({
   selector: 'app-view-playlist',
@@ -6,11 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-playlist.component.sass']
 })
 export class ViewPlaylistComponent implements OnInit {
-  constructor() { }
+  songs: Song[] = [];
+  private _userId: number;
+
+  constructor(
+    private _songService: SongsService,
+    private _authService: AuthService,
+    private _reactionService: ReactionService
+  ) {}
 
   ngOnInit(): void {
+    this.getUserId();
   }
+
   nextSlide = () => {};
 
   previousSlide = () => {};
+
+  getUserId() {
+    this._authService.getAuthStateObservable()
+      .pipe(filter((state) => state !== null))
+      .subscribe((authState) => {
+        this._userId = authState!.id;
+      });
+  }
+
+  dislikeSong(songId: number) {
+    this._reactionService.removeLike(songId, this._userId)
+      .subscribe({
+        next: () => {
+          this.songs = this.songs.filter((s) => s.id !== songId);
+        }
+      });
+  }
+
+  play = () => {};
 }
