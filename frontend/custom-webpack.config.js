31b4cf8589215ada6c2cd0d9d7eb58d284a8e8b2
krolebord <53054887+krolebord@users.noreply.github.com>
2021-08-12 13:02:12 +0300

    Add firebase api key to env (#65)

--- frontend/custom-webpack.config.js
+++ frontend/custom-webpack.config.js
@@ -3,8 +3,8 @@ const webpack = require('webpack');
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-     'process.env':{
-         apiKey:JSON.stringify(process.env.FIREBASE_KEY),
+     'process.env': {
+       FIREBASE_KEY: JSON.stringify(process.env.FIREBASE_KEY),
      }
     })
   ]
