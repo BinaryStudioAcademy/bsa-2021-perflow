6c2826b067ff473b3247c9460e569cba352218b4
Den313 <37304507+Den313@users.noreply.github.com>
2021-08-13 14:19:28 +0300

    Fixed links to artist page

--- frontend/src/app/modules/main/cards/new-release-card/new-release-card/new-release-card.component.ts
+++ frontend/src/app/modules/main/cards/new-release-card/new-release-card/new-release-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { AlbumAuthor } from 'src/app/models/user/album-author';
 
 @Component({
@@ -6,8 +7,20 @@ import { AlbumAuthor } from 'src/app/models/user/album-author';
   templateUrl: './new-release-card.component.html',
   styleUrls: ['./new-release-card.component.sass']
 })
-export class NewReleaseCardComponent {
+export class NewReleaseCardComponent implements OnInit{
   @Input() name: string;
   @Input() authors: Array<AlbumAuthor>;
   @Input() iconUrl: string;
+
+  public baseUrl: string;
+
+  constructor(
+    private _location: PlatformLocation 
+  ) {
+  };
+
+  public ngOnInit(){
+    this.baseUrl = `${this._location.hostname}:${this._location.port}/`;
+    console.log(`${this.baseUrl}artists/${this.authors[0].id}`);
+  }
 }
