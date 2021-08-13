38c951865ae35985b81b16d4f15aa1cc783d5f62
sleepergoose <sleepergoose@gmail.com>
2021-08-13 10:06:47 +0300

    Added logic for user icon load

--- frontend/src/app/modules/main/profile-menu/profile-menu.component.ts
+++ frontend/src/app/modules/main/profile-menu/profile-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { filter } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 import { AuthService } from '../../../services/auth/auth.service';
 import { UserRoles } from '../../../models/enums/user-roles.enum';
 
@@ -12,21 +13,34 @@ export class ProfileMenuComponent {
   isProfileMenuShown: boolean = false;
 
   userName: string = 'user';
-
-  // Determines if the menu item 'Perflow Studio' will be shown in Profile menu
-  // 'Perflow Studio' - available only for artists and moderators
+  userIconURL: string = '';
   isRightRole: boolean = false;
 
+  private _userId: number;
+
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _userService: UserService
   ) {
     this._authService.getAuthStateObservable()
       .pipe(filter((state) => state !== null))
       .subscribe((authState) => {
         this.userName = authState!.userName;
+        this._userId = authState!.id;
+
+        this.getUserIconURL();
 
         const { role } = authState!;
         this.isRightRole = role === UserRoles.artist || role === UserRoles.moderator;
+      });
+  }
+
+  getUserIconURL() {
+    this._userService.getUser(this._userId)
+      .subscribe({
+        next: (user) => {
+          this.userIconURL = user.iconURL;
+        }
       });
   }
 
