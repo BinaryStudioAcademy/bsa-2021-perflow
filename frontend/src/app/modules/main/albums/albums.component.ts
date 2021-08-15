c426037441ac22ecd8ed18511395e6c9cfe13d88
Khrapal Maksym <72307872+Khrapal-Max@users.noreply.github.com>
2021-08-16 01:43:42 +0300

    done

--- frontend/src/app/modules/main/albums/albums.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-albums',
-  templateUrl: './albums.component.html',
-  styleUrls: ['./albums.component.sass']
-})
-export class AlbumsComponent {
-}
