799991474886209c74b0c501630b713d2abb05e4
Krolebord <krolebord@gmail.com>
2021-08-17 20:05:07 +0300

    Split ocelot configuration into multiple files

---
+++ backend/ApiGateway/ApiGateway/Exceptions/OcelotConfigurationException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ApiGateway.Exceptions
+{
+    public class OcelotConfigurationException : Exception
+    {
+        public OcelotConfigurationException(string message) : base(message) { }
+    }
+}
