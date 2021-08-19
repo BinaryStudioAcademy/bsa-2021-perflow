bce727ed37caf3d839fd4dc7f2147394ba3f3856
Jeki <ambrejine00@gmail.com>
2021-08-19 11:54:13 +0300

    Create Add song route

---
+++ backend/Perflow/Controllers/RecentlyPlayedController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Perflow.Common.DTO.RecentlyPlayed;
+using Perflow.Services.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Perflow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecentlyPlayedController : ControllerBase
+    {
+        RecentlyPlayedService _service;
+        public RecentlyPlayedController(RecentlyPlayedService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("add")]
+        public async Task<ActionResult> AddSong([FromBody] RecentlyPlayedDTO rpInfo)
+        {
+            await _service.AddSong(rpInfo);
+
+            return Ok();
+        }
+    }
+}
---
+++ backend/Perflow/Services/Implementations/RecentlyPlayedService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Perflow.Common.DTO.RecentlyPlayed;
+using Perflow.DataAccess.Context;
+using Perflow.Domain;
+using Perflow.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Perflow.Services.Implementations
+{
+    public class RecentlyPlayedService : BaseService
+    {
+        private const int maxNumberOfStoredSongsPerUser = 100;
+        public RecentlyPlayedService(PerflowContext context, IMapper mapper) : base(context, mapper)
+        { }
+
+        public async Task AddSong(RecentlyPlayedDTO rpInfo)
+        {
+            var rpList = context.RecentlyPlayed.Where(rp => rp.UserId == rpInfo.UserId);
+
+            var rpUpdate = rpList.Where(rp => rp.ArtistId == rpInfo.ArtistId &&
+                                              rp.AlbumId == rpInfo.AlbumId &&
+                                              rp.PlaylistId == rpInfo.PlaylistId &&
+                                              rp.SongId == rpInfo.SongId)
+                                 .FirstOrDefault();
+
+            if(rpUpdate == null)
+            {
+                if (rpList.Count() >= maxNumberOfStoredSongsPerUser)
+                {
+                    var rpRemove = rpList
+                                    .OrderByDescending(rp => rp.LastTimeListened)
+                                    .Last();
+                    context.RecentlyPlayed.Remove(rpRemove);
+                }
+
+                var newRp = mapper.Map<RecentlyPlayed>(rpInfo);
+                await context.RecentlyPlayed.AddAsync(newRp);
+            }
+            else
+            {
+                rpUpdate.Frequency++;
+                rpUpdate.LastTimeListened = DateTime.Now;
+                context.Entry(rpUpdate).State = EntityState.Modified;
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task GetAll(int userId)
+        {
+            var rpList = await context.RecentlyPlayed
+                                .Where(rp => rp.UserId == userId)
+                                .ToListAsync();
+
+            return;
+        }
+    }
+}
