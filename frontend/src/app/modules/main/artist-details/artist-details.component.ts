83187b66d60c89a29da7e0506d80373ce9ddc57d
Slawisch <dmitry.js01@gmail.com>
2021-08-20 19:18:14 +0300

    User data changes on loading component's url

--- frontend/src/app/modules/main/artist-details/artist-details.component.ts
+++ frontend/src/app/modules/main/artist-details/artist-details.component.ts
@@ -47,7 +47,11 @@ export class ArtistDetailsComponent implements OnInit {
     private _reactionService: ReactionService,
     private _authService: AuthService,
     private _albumsService: AlbumService
-  ) { }
+  ) { 
+    _route.params.subscribe(routeParams => {
+      this.loadData();
+  });
+  }
 
   ngOnInit() {
     this.loadData();
