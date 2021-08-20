01c90b702620f9172795a8d1cba11d938b71936b
Jeki <ambrejine00@gmail.com>
2021-08-20 14:15:55 +0300

    Generate new module

---
+++ frontend/src/app/modules/main-content-container/main-content-container.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProfileMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainContentContainerModule { }
