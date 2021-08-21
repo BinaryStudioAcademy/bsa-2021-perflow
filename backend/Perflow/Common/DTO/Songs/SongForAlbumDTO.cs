ce83e6cfced757ee7617d8a22b90becf5c833f4a
alls77 <alina_selina@bk.ru>
2021-08-21 11:53:12 +0300

    update snackbar; fix layout (#155)
    * update snackbar
    
    * fix layout
    
    * hide like icon for the author (album)
    
    * update snackbar (login page, registration page)
    
    * fix formatting
    
    * small fix

---
+++ backend/Perflow/Common/DTO/Songs/SongForAlbumDTO.cs
@@ -0,0 +1,20 @@
+﻿using Perflow.Common.DTO.Groups;
+using Perflow.Common.DTO.Users;
+using Perflow.Common.DTO.Albums;
+using System;
+
+namespace Perflow.Common.DTO.Songs
+{
+    public class SongForAlbumDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ArtistForAlbumDTO Artist { get; set; }
+        public GroupForAlbumDTO Group { get; set; }
+        public AlbumForPlaylistDTO Album { get; set; }
+        public int Duration { get; set; }
+        public bool HasCensorship { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public bool IsLiked { get; set; }
+    }
+}
