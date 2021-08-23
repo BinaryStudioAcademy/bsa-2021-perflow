b5d1f8d0b4734e0a3fc296c0456a1efa93815025
Den313 <37304507+Den313@users.noreply.github.com>
2021-08-23 15:41:18 +0300

    Add DTO for request

---
+++ frontend/src/app/models/song/song-liked.ts
@@ -0,0 +1,12 @@
+import { AlbumName } from "../album/album-name";
+import { AlbumAuthor } from "../user/album-author";
+
+export interface SongLiked {
+  id: number;
+  name: string;
+  duration: number;
+  hasCensorship: boolean;
+  author: AlbumAuthor;
+  album: AlbumName;
+  isLiked: boolean;
+}
