0c5b1b114dac2c03b3382ecfdf48ddf1a2510170
Den313 <37304507+Den313@users.noreply.github.com>
2021-08-25 22:50:20 +0300

    Create models and DTO's for ArtistApplicant

---
+++ backend/Perflow/Common/DTO/Users/ArtistApplicantDTO.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Perflow.Common.DTO.Users
+{
+    public class ArtistApplicantDTO
+    {
+        public string Email { get; set; }
+        public int UserRole { get; set; }
+    }
+}
