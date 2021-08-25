bea6bc38c6877078fe5a1c6617bc61cb68e1a4ee
Jeki <ambrejine00@gmail.com>
2021-08-25 10:46:13 +0300

    Fix remarks

--- frontend/src/app/modules/perflow-studio/playlists-page/playlists-page.component.ts
+++ frontend/src/app/modules/perflow-studio/playlists-page/playlists-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { filter } from 'rxjs/operators';
 import { PlaylistView } from 'src/app/models/playlist/playlist-view';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -9,18 +9,21 @@ import { PlaylistsService } from 'src/app/services/playlists/playlist.service';
   templateUrl: './playlists-page.component.html',
   styleUrls: ['./playlists-page.component.sass']
 })
-export class PlaylistsPageComponent {
+export class PlaylistsPageComponent implements OnInit {
   playlists: PlaylistView[] = [];
-  userId: number = 1;
+  userId: number = -1;
 
   constructor(
     private _authService: AuthService,
     private _playlistService: PlaylistsService
   ) {
+  }
+
+  ngOnInit(): void {
     this._authService.getAuthStateObservableFirst()
       .pipe(filter((state) => !!state))
       .subscribe((authState) => {
-        this.userId = authState!.id;
+        this.userId = authState!.id
       });
 
     this.loadPlaylist();
