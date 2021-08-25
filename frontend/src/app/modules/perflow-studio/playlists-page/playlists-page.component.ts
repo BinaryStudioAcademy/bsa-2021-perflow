24d4f460af1aede8f24d511f46e2c9c37c6f131f
Jeki <ambrejine00@gmail.com>
2021-08-25 08:00:55 +0300

    Load and display playlists

--- frontend/src/app/modules/perflow-studio/playlists-page/playlists-page.component.ts
+++ frontend/src/app/modules/perflow-studio/playlists-page/playlists-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { PlaylistView } from 'src/app/models/playlist/playlist-view';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { PlaylistsService } from 'src/app/services/playlists/playlist.service';
 
 @Component({
   selector: 'app-playlists-page',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./playlists-page.component.sass']
 })
 export class PlaylistsPageComponent {
+  playlists: PlaylistView[] = [];
+  userId: number = 1;
 
+  constructor(
+    private _authService: AuthService,
+    private _playlistService: PlaylistsService
+  ) {
+    this._authService.getAuthStateObservableFirst()
+      .pipe(filter((state) => !!state))
+      .subscribe((authState) => {
+        this.userId = authState!.id;
+      });
+
+    this.loadPlaylist();
+  }
+
+  loadPlaylist() {
+    this._playlistService.getPlaylistsByAuthorId(this.userId)
+      .subscribe((ps) => {
+        this.playlists = ps;
+      });
+  }
 }
