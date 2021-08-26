a47ed62a8def3069812f03df827c944feeb3bb88
Slawisch <dmitry.js01@gmail.com>
2021-08-26 16:24:15 +0300

    Add library pages and navigation

---
+++ mobile/lib/screens/main/library/library_albums_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LibraryAlbumsScreen extends StatelessWidget {
+  const LibraryAlbumsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Coming soon...",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
--- mobile/lib/screens/main/library/library_screen.dart
+++ mobile/lib/screens/main/library/library_screen.dart
@@ -1,65 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:perflow/cubits/library_navigation/library_navigation_cubit.dart';
+import 'package:perflow/routes.dart';
 import 'package:perflow/theme.dart';
+import 'package:vrouter/vrouter.dart';
 
 class LibraryScreen extends StatelessWidget {
-  const LibraryScreen({Key? key}) : super(key: key);
+  final Widget child;
+
+  const LibraryScreen({required this.child, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-            child: SingleChildScrollView(child: _topNavmenu()),
-          ),
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  _topNavmenu(),
+                ],
+              ),
+            ),
+            Expanded(child: child)
+          ],
         ),
       ),
     );
   }
 
   Widget _topNavmenu() {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 4),
-          child: Row(
+    return Container(
+        child: BlocBuilder<LibraryNavigationCubit, LibraryNavigationState>(
+            builder: (context, state) {
+          return Row(
             children: [
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "All",
-                  style: TextStyle(
-                      color: Perflow.textColor,
-                      fontSize: 18,
-                      fontFamily: Perflow.title),
-                ),
+              _LibraryNavButton(
+                onTap: () {
+                  context.vRouter.to(Routes.libraryAll);
+                },
+                title: "All",
+                selected: state is LibraryNavigationAll,
               ),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Albums",
-                  style: TextStyle(
-                      color: Perflow.textColor,
-                      fontSize: 18,
-                      fontFamily: Perflow.title),
-                ),
+              _LibraryNavButton(
+                onTap: () {
+                  context.vRouter.to(Routes.libraryAlbums);
+                },
+                title: "Albums",
+                selected: state is LibraryNavigationAlbums,
               ),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Artists",
-                  style: TextStyle(
-                      color: Perflow.textColor,
-                      fontSize: 18,
-                      fontFamily: Perflow.title),
-                ),
+              _LibraryNavButton(
+                onTap: () {
+                  context.vRouter.to(Routes.libraryArtists);
+                },
+                title: "Artists",
+                selected: state is LibraryNavigationArtists,
               ),
             ],
+          );
+        }),
+        decoration: const BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: Color(0x3421292D),
+            spreadRadius: 7,
+            blurRadius: 5,
+            offset: Offset(0, 0), // changes position of shadow
+          ),
+        ], color: Perflow.surfaceColor));
+  }
+}
+
+class _LibraryNavButton extends StatelessWidget {
+  final bool selected;
+  final String title;
+  final void Function() onTap;
+
+  const _LibraryNavButton({
+    required this.selected,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = selected ? Perflow.primaryLightColor : Perflow.textColor;
+
+    return Expanded(
+      child: InkResponse(
+        onTap: onTap,
+        child: TextButton(
+          onPressed: () {
+            onTap();
+          },
+          child: Text(
+            title,
+            style: TextStyle(
+                color: color, fontSize: 15, fontFamily: Perflow.title, fontWeight: FontWeight.normal),
           ),
         ),
-        const Center(child: Text('Library screen'))
-      ],
+      ),
     );
   }
 }
