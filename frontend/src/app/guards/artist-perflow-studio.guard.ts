ab0d10ebdb2996f88200794acd8eb884221ddbbb
alina selina <sp115-27@stu.zsea.edu.ua>
2021-08-30 22:07:10 +0300

    add guards

---
+++ frontend/src/app/guards/artist-perflow-studio.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { AuthGuard } from './auth.guard';
+import { UserRoles } from '../models/enums/user-roles.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModeratorPerflowStudioGuard extends AuthGuard {
+  protected canActivateObservable() {
+    return this.authService.getAuthStateObservable()
+      .pipe(
+        map((state) => {
+          if (state?.role === UserRoles.artist || state?.role === UserRoles.teamMember) {
+            return true;
+          }
+
+          return this.router.parseUrl('/perflowstudio');
+        })
+      );
+  }
+}
--- frontend/src/app/modules/perflow-studio/left-side-menu/left-side-menu.component.ts
+++ frontend/src/app/modules/perflow-studio/left-side-menu/left-side-menu.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
+import { UserRoles } from 'src/app/models/enums/user-roles.enum';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-left-side-menu',
   templateUrl: './left-side-menu.component.html',
   styleUrls: ['./left-side-menu.component.sass']
 })
-export class LeftSideMenuComponent {
+export class LeftSideMenuComponent implements OnDestroy {
+  private _unsubscribe$ = new Subject<void>();
+  userRole: UserRoles;
+  userRoles = UserRoles;
 
+  constructor(private _authService: AuthService) {
+    this.getUserRole();
+  }
+
+  getUserRole() {
+    this._authService.getAuthStateObservable()
+      .pipe(
+        takeUntil(this._unsubscribe$),
+        filter((state) => !!state))
+      .subscribe((state) => {
+        this.userRole = state!.role;
+      });
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+  }
 }
