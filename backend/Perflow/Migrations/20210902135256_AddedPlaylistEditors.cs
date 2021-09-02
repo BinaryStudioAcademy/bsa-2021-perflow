539a6683b6b71c45c3c4b1e71c0b4fba43a38c11
proskurnicha <proskurnicha@gmail.com>
2021-09-02 16:56:28 +0300

    Bugfix/migrations order (#250)
    * Fixed migrations.
    
    * Added migration.

---
+++ backend/Perflow/Migrations/20210902135256_AddedPlaylistEditors.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Perflow.Migrations
+{
+    public partial class AddedPlaylistEditors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlaylistEditors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlaylistId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlaylistEditors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlaylistEditors_Playlists_PlaylistId",
+                        column: x => x.PlaylistId,
+                        principalTable: "Playlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlaylistEditors_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlaylistEditors_PlaylistId",
+                table: "PlaylistEditors",
+                column: "PlaylistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlaylistEditors_UserId",
+                table: "PlaylistEditors",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlaylistEditors");
+        }
+    }
+}
