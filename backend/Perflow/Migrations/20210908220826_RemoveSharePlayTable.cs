1b527645c4dd4b50df342852f1e5f3897f6df7bf
sleepergoose <sleepergoose@gmail.com>
2021-09-09 01:12:14 +0300

    Removed SharePlayTable

---
+++ backend/Perflow/Migrations/20210908220826_RemoveSharePlayTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Perflow.Migrations
+{
+    public partial class RemoveSharePlayTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SharePlay");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SharePlay",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MasterId = table.Column<int>(type: "int", nullable: false),
+                    PlaylistId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharePlay", x => x.Id);
+                });
+        }
+    }
+}
